19c4fa85050cc273c350c476ac76c92e2d705bae
Najib Zaghly <najib.zaghly@enim.ac.ma>
2022-09-25 17:24:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Post from './Post'
+import './App.css';
+
+function App() {
+    return ( <div className = "App" >
+        <Post key = '1'
+
+        text = 'dgchgfhgvhgvbjh jhbhb kuhuk'
+        user = 'username'
+        image = 'https://i.kym-cdn.com/photos/images/original/001/468/202/b02.jpg' /
+        >
+        </div>
+    );
+}
+
+export default App;
