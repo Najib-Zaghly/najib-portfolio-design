919f2fd8096d93d17738451b7a6d5d3e21c4e7b5
Najib Zaghly <najib.zaghly@enim.ac.ma>
2022-10-26 01:36:50 +0200

    najib portfolio

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { themeContext } from "../../Context";
+import "./Experience.css";
+const Experience = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="experience" id='experience'>
+      <div className="achievement">
+        {/* darkMode */}
+       
+    </div>
+    </div>
+  );
+};
+
+export default Experience;
